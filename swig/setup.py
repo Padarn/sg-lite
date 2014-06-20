7856631bfa07a857bb187f5b88bb89a4d67098e5
Padarn Wilson <padarn@gmail.com>
2014-06-20 21:14:35 +1000

    BUG: attempting to fix build

--- swig/setup.py
+++ swig/setup.py
@@ -3,13 +3,19 @@
 from distutils.core import setup, Extension
 
 import numpy
+import os
+
 numpyinclude = numpy.__file__[:-12]+'core/include/'
+#libfullpath = os.path.realpath('../sparsegrid/libsg-lite.so')
+libpath = os.path.realpath('../sparsegrid/')
+
 
 sparsepad = Extension('_sparsepad',
                            sources=['sparsepad_wrap.cxx'],
-                           extra_objects=['../sparsegrid/libsg-lite.so'],
+                           #extra_objects=[libfullpath],
+                           libraries=['sg-lite'],
                            include_dirs=['../',numpyinclude],
-                           library_dirs=['../sparsegrid']
+                           library_dirs=[libpath],
                            )
 
 setup (name = 'sparsepad',
