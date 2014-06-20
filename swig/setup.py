102beb0dbf07ef2f6c3999b3649ce435d288fc4c
Padarn Wilson <padarn@gmail.com>
2014-06-20 21:43:04 +1000

    FIX: Build should now work on linux and osx

--- swig/setup.py
+++ swig/setup.py
@@ -6,13 +6,11 @@ import numpy
 import os
 
 numpyinclude = numpy.__file__[:-12]+'core/include/'
-#libfullpath = os.path.realpath('../sparsegrid/libsg-lite.so')
 libpath = os.path.realpath('../sparsegrid/')
 
 
 sparsepad = Extension('_sparsepad',
                            sources=['sparsepad_wrap.cxx'],
-                           #extra_objects=[libfullpath],
                            libraries=['sg-lite'],
                            include_dirs=['../',numpyinclude],
                            library_dirs=[libpath],
