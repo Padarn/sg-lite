e63677c67c3c8cda81654736b2d9e0c3fef90c00
Padarn Wilson <padarn@gmail.com>
2014-06-21 11:13:39 +1000

    rpath for non darwin

--- swig/setup.py
+++ swig/setup.py
@@ -4,16 +4,22 @@ from distutils.core import setup, Extension
 
 import numpy
 import os
+import sys
 
 numpyinclude = numpy.__file__[:-12]+'core/include/'
 libpath = os.path.realpath('../sparsegrid/')
-
+platform = sys.platform
+if platform != 'darwin':
+    runtime = '-rpath ' + libpath
+else:
+    runtime = ''
 
 sparsepad = Extension('_sparsepad',
                            sources=['sparsepad_wrap.cxx'],
                            libraries=['sg-lite'],
                            include_dirs=['../',numpyinclude],
                            library_dirs=[libpath],
+                           extra_objects=[runtime]
                            )
 
 setup (name = 'sparsepad',
